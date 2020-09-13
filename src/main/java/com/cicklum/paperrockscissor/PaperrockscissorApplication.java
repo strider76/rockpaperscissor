22e69b6ab5de817f04ef5d79f55ed7a03846e78e
manolo <manuel@betangible.com>
2020-09-13 12:30:08 +0200

    init() - template project

---
+++ src/main/java/com/cicklum/paperrockscissor/PaperrockscissorApplication.java
@@ -0,0 +1,13 @@
+package com.cicklum.paperrockscissor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperrockscissorApplication {
+
+    public static void main(String[] args) {
+	SpringApplication.run(PaperrockscissorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cicklum/paperrockscissor/PaperrockscissorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicklum.paperrockscissor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperrockscissorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
